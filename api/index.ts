52dc81e24e64ff510d9f3cd8e2b25e030073029e
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-23 21:23:39 +0200

    Validate the URL before.. well... doing stuff

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,18 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getLinkPreview } from "link-preview-js";
 
+import isURL from 'validator/lib/isURL';
+
 export default (request: VercelRequest, response: VercelResponse) => {
   const { url = "https://trnck.dev/" } = request.query;
-  getLinkPreview(url).then((data) => {
-    response.status(200).send(data);
-  });
+  if (isURL(url)) {
+    getLinkPreview(url).then((data) => {
+        response.status(200).send(data);
+      });
+  } else {
+    response.status(200).send({
+        status: "error",
+        message: "Invalid URL provided"
+    })
+  }
 };
