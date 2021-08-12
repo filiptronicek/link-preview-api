84229a8f2815820c80d907704bae7c2978b939be
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-12 17:24:48 +0200

    Types & stuff

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,27 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getLinkPreview } from "link-preview-js";
 
-import isURL from 'validator/lib/isURL';
+import isURL from "validator/lib/isURL";
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const { url = "https://github.com/" } = request.query;
-  if (isURL(url)) {
-    getLinkPreview(url).then((data) => {
+  const { url } = request.query;
+  if (url) {
+    const stringifiedURL = url.toString();
+    if (isURL(stringifiedURL)) {
+      getLinkPreview(stringifiedURL).then((data) => {
         response.status(200).send(data);
       });
+    } else {
+      response.status(400).send({
+        status: "error",
+        message: "Invalid URL provided",
+      });
+    }
   } else {
     response.status(400).send({
-        status: "error",
-        message: "Invalid URL provided"
-    })
+      status: "error",
+      message:
+        "No URL provided, to start making requests, refer to https://github.com/filiptronicek/link-preview-api#how-to-use-this-api",
+    });
   }
 };
